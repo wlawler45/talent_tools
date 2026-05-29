084fbfd2088b953d41af4961872f9e55844f0898
wlawler45 <williamgmlawler@yahoo.com>
2026-05-28 21:39:28 -0400

    Refactor FastaIterator to remove generic_dna dependency
    Removed the use of 'generic_dna' from FastaIterator calls.

--- findTAL.py
+++ findTAL.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from Bio.Alphabet import generic_dna
+#from Bio.Alphabet import generic_dna
 
 from talconfig import BASE_DIR, GENOME_FILE, PROMOTEROME_FILE, VALID_GENOME_ORGANISMS, VALID_PROMOTEROME_ORGANISMS, OFFTARGET_COUNTING_SIZE_LIMIT
 from talutil import validate_options_handler, OptParser, FastaIterator, create_logger, check_fasta_pasta, OptionObject, TaskError, reverseComplement, Conditional
@@ -192,7 +192,7 @@ def validateOptions(options):
             
             gene_length_total = 0
             
-            for gene in FastaIterator(seq_file, alphabet=generic_dna):
+            for gene in FastaIterator(seq_file):
                 gene_length_total += len(gene.seq)
             
             #if gene_length_total > 1000:
@@ -222,7 +222,7 @@ def RunFindTALTask(options):
                 
                 check_fasta_pasta(maybe_entrez_file.file)
                 
-                for record in FastaIterator(maybe_entrez_file.file, alphabet=generic_dna):
+                for record in FastaIterator(maybe_entrez_file.file):
                     if len(record.seq) > OFFTARGET_COUNTING_SIZE_LIMIT:
                         raise TaskError("Off-Target counting is only supported for NCBI records where all individual sequences are under %d megabases in size" % (OFFTARGET_COUNTING_SIZE_LIMIT / 1000000))
             
@@ -290,7 +290,7 @@ def RunFindTALTask(options):
         
         binding_sites = []
         
-        for gene in FastaIterator(seq_file, alphabet=generic_dna):
+        for gene in FastaIterator(seq_file):
             
             sequence = str(gene.seq).upper()
             
